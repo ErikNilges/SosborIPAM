6c73b1cc3057b9d4c9c3fecb8754ed9a92df84cb
ErikNilges <erik.nilges@gmail.com>
2017-01-13 09:06:54 +0100

    Passwörter werden gehasht

--- src/login_cmd.php
+++ src/login_cmd.php
@@ -3,22 +3,18 @@ session_start();
 
 include 'db.php';
 
-$username = $_POST['username'];
-$password = $_POST['password'];
 
-//$passhash = password_hash('$password', PASSWORD_BCRYPT);
-
-$sql = "SELECT * FROM user WHERE username='$username' AND password='$password'";
-
+$sql = "SELECT * FROM user WHERE username='{$_POST['username']}'";
 $result = mysqli_query($db, $sql);
+$row = mysqli_fetch_assoc($result);
 
-if ($row = mysqli_fetch_assoc($result)){
+
+if (password_verify($_POST['password'], $row['password'])){
   $_SESSION['username'] = $row['username'];
   $_SESSION['vorname'] = $row['vorname'];
   $_SESSION['nachname'] = $row['nachname'];
-  header("Location: index.html");
+  header("Location: index.php");
 } else {
   header("Location: login.php");
 }
-// header("Location: index.html")
  ?>
--- src/new_user_cmd.php
+++ src/new_user_cmd.php
@@ -8,9 +8,9 @@ $nachname = $_POST['nachname'];
 $username = $_POST['username'];
 $password = $_POST['password'];
 
-//$passhash = password_hash('$password', PASSWORD_BCRYPT);
+$pwdhash = password_hash($password, PASSWORD_BCRYPT);
 
-$sql = "INSERT INTO user (vorname, nachname, username, password) VALUES ('$vorname', '$nachname', '$username', '$password')";
+$sql = "INSERT INTO user (vorname, nachname, username, password) VALUES ('$vorname', '$nachname', '$username', '$pwdhash')";
 
 $result = mysqli_query($db, $sql);
 
