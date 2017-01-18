68e55d5cc56bde9fe94fbb41dd9bc476a08d7f57
Shadyy1337 <fr4gg1ng@msn.com>
2017-01-18 09:59:58 +0100

    Add files via upload

---
+++ src/impressum.php
@@ -0,0 +1,10 @@
+
+<h2>Impressum</h2>
+<p>Sosbor IPAM<br />
+Highskilled Boulevard 1<br />1337 Wiesbaden</p>
+<p>Telefon: 0611/1235512<br />
+Telefax: 0611/1235513<br />
+E-Mail: <a href="mailto:kontakt@SosborIPAM.com">kontakt@SosborIPAM.com</a><br />
+</p>
+<p><strong>Vertreten durch:</strong><br />Sosbor Group<br />Geschäftsführer: Alexander Schneider<br />Registergericht: Amtsgericht Wiesbaden<br />Registernummer: 5518123758123RT3123</p><p><strong>Registereintrag:</strong><br />Eingetragen im Handelsregister.<br />Registergericht: Amstgericht Wiesbaden<br />Registernummer: 123554123RT1234</p><p><strong>Umsatzsteuer-ID: </strong><br />Umsatzsteuer-Identifikationsnummer nach §27a Umsatzsteuergesetz:<br />12355512355</p><p><strong>Wirtschafts-ID: </strong>Wirtschafts-IdNr: 3451235531235</p><strong>Aufsichtsbehörde: </strong>Wiesbadener Aufsichtbehörde<br /><p>
+
